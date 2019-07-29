bfec820f84534972f5b59ca2698c36edc19a95f3
Iman Nurohman <imannurohman1@gmail.com>
2019-07-29 11:06:07 +0700

    Grid dan Constraint

---
+++ app/src/main/java/com/belajar/griddanconstraint/GridActivity.kt
@@ -0,0 +1,12 @@
+package com.belajar.griddanconstraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GridActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.grid_layout)
+    }
+}
